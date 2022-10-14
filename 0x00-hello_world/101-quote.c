31c5fb18ac568f2ccc23f8e04d12748256716270
Rolain Djeumen <uprightech@gmail.com>
2022-10-14 01:03:03 +0100

    minor bug fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,10 @@
 #include <unistd.h>
 
 #define STDOUT_FILE_DESCRIPTOR 2
-/**
-* main - Entry point
-*
-* Return: Always 1 (Failed)
-*/
+#define MESSAGE_TO_PRINT "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
 int main(void)
 {
-	write(STDOUT_FILE_DESCRIPTOR, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+  write(STDOUT_FILE_DESCRIPTOR, MESSAGE_TO_PRINT, 59);
+  return (1);
 }
