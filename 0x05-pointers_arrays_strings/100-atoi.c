c1c1536a45ccadb0045683b638953fd5dc7fe878
Rolain Djeumen <uprightech@gmail.com>
2022-10-25 01:06:36 +0100

    betty style fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,13 +11,7 @@ int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
+	i = d = n = len = f = digit = 0;
 	while (s[len] != '\0')
 		len++;
 
