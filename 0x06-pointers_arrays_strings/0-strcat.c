aaab04359f27f1128ef0ed680961aedb0a6e9fef
Rolain Djeumen <uprightech@gmail.com>
2022-10-26 05:16:02 +0100

    added code for strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * strcat - concatenates two strings, appending src at the end of dest
+ * @dest - pointer to the destination string where src will be copied 
+ * @src  - pointer to the source string to copy
+ * Return: a pointer to the destination string 
+ */
+char * _strcat(char * dest, char * src)
+{
+    char * ret = dest;
+
+    if (src == NULL || dest == NULL)
+        return (ret);
+    while(*dest != '\0')
+    {
+        dest++;
+    }
+
+    while(*src++ != '\0')
+    {
+        *dest++ = *(src - 1);
+    }
+    *dest = '\0';
+    return (ret);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef __MAIN_H_INCLUDED__
+#define __MAIN_H_INCLUDED__
+
+#include <stdio.h>
+
+char * _strcat(char * dest, char * src);
+#endif
