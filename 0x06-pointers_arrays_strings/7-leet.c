240290fd165f95ccb34eb820233f847263263734
Rolain Djeumen <uprightech@gmail.com>
2022-10-27 00:53:17 +0100

    betty style fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,17 +6,18 @@
  */
 char *leet(char *ch)
 {
-    int i = 0, j;
-    char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int i = 0, j;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-    while (ch[i])
-    {
-        for (j = 0; j <= 7; j++)
-        {
-            if (ch[i] == leet[j] || ch[i] - 32 == leet[j])
-                ch[i] = j + '0';
-        }
-        i++;
-    }
-    return (ch);
+	while (ch[i])
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			if (ch[i] == leet[j] || ch[i] - 32 == leet[j])
+				ch[i] = j + '0';
+		}
+		i++;
+	}
+	return (ch);
 }
+
