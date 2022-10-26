2e11b4c50977615d4a1404293cfd22982bbc84b4
Rolain Djeumen <uprightech@gmail.com>
2022-10-26 21:01:48 +0100

    strcat implementation fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,25 @@
 #include "main.h"
-
 /**
- * strcat - concatenates two strings, appending src at the end of dest
- * @dest - pointer to the destination string where src will be copied 
- * @src  - pointer to the source string to copy
- * Return: a pointer to the destination string 
- */
-char * _strcat(char * dest, char * src)
+* _strcat - concatenates two strings, appending src at the end of dest
+* @dest: pointer to the destination string where src will be copied
+* @src: pointer to the source string to copy
+* Return: a pointer to the destination string
+*/
+char *_strcat(char *dest, char *src)
 {
-    char * ret = dest;
+	char *ret = dest;
 
-    if (src == NULL || dest == NULL)
-        return (ret);
-    while(*dest != '\0')
-    {
-        dest++;
-    }
+	if (src == NULL || dest == NULL)
+		return (ret);
+	while (*dest != '\0')
+	{
+		dest++;
+	}
 
-    while(*src++ != '\0')
-    {
-        *dest++ = *(src - 1);
-    }
-    *dest = '\0';
-    return (ret);
+	while (*src++ != '\0')
+	{
+		*dest++ = *(src - 1);
+	}
+	*dest = '\0';
+	return (ret);
 }
