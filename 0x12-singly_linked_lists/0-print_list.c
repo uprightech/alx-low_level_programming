b5cd714fa7e6c4fbee2a3b8d14c9e301fee29f14
Rolain Djeumen <uprightech@gmail.com>
2022-11-24 22:31:59 +0100

    fix null printing

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,14 @@ size_t print_list(const list_t *h)
 
 	for (i = 0; h; i++)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
 		h = h->next;
 	}
 	return (i);
