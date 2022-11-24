8e1c44b3310d0ab4b9f767f5249b2d5808f4b2c8
Rolain Djeumen <uprightech@gmail.com>
2022-11-24 22:13:40 +0100

    singly linked list

---
+++ 0x12-singly_linked_lists/mains/main.3.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    list_t *head;
+
+    head = NULL;
+    add_node_end(&head, NULL);
+    add_node_end(&head, "Colton");
+    print_list(head);
+    return (0);
+}
